c88ddab29f236e5e5e06ddee9721c1d536467ccd
rarararaitaisou <hokuto_mb@yahoo.co.jp>
2022-08-10 06:19:04 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a className={styles.anchor}>Index</a>
+      </Link>
+      <Link href="/about">
+        <a className={styles.anchor}>About</a>
+      </Link>
+    </header>
+  );
+}
